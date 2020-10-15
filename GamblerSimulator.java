072c0113ce7516fade7adaaf3d725a723e05a1b4
anushka <anushka25999@gmail.com>
2020-10-15 09:46:19 +0530

    UC1 added to GamblerSimulator.java

--- GamblerSimulator.java
+++ GamblerSimulator.java
@@ -1,5 +1,10 @@
 public class GamblerSimulator{
+	GamblerSimulator(){
+		int total = 100;
+		int bet = 1;
+	}
 	public static void main(String args[]){
 		System.out.println("Welcome to the Gambler Simulator Problem");
+		GamblerSimulator stake = new GamblerSimulator();
 	}
 }
