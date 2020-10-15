a797fe64ac6e23a991900579601105ab017ac79d
anushka <anushka25999@gmail.com>
2020-10-15 13:11:22 +0530

    UC3 added to GamblerSimulator.java

--- GamblerSimulator.java
+++ GamblerSimulator.java
@@ -1,20 +1,45 @@
 public class GamblerSimulator{
 
 	//Constants
-	private final int TOTAL = 100;
+	private final int STAKE = 100;
 	private final int BET = 1;
+	private static final float STAKE_VALUE = 0.5f;
+	public int winningAmount, losingAmount, stake;
 
-	public void winOrLose(){
+	//UC2 WIN OR LOSS
+	public void winOrLoss(){
 		int random=(int)Math.floor(Math.random()*10)%2;
 		if(random==0){
-			System.out.println("You Won");
+			System.out.println("You Win");
+			stake++;
 		}
 		else{
-			System.out.println("You Lose");
+			System.out.println("You Loss");
+			stake--;
 		}
 	}
+
+	//UC3 Gambler Resigns after 50% win or loss
+	public int resignGame(){
+		losingAmount = (int)Math.round(STAKE * STAKE_VALUE);
+		winningAmount = (int)Math.round(STAKE+(STAKE * STAKE_VALUE));
+		boolean stop = true;
+		stake = STAKE;
+		while(stop == true){
+			winOrLoss();
+			if(stake == losingAmount){
+				stop=false;
+			}
+			if(stake == winningAmount){
+				stop=false;
+			}
+		}
+		return stake;
+	}
+
 	public static void main(String[] args){
 		GamblerSimulator gamble = new GamblerSimulator();
-		gamble.winOrLose();
+		gamble.winOrLoss();
+		System.out.println(gamble.resignGame());
 	}
 }
