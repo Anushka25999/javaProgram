bd95c4a7e9b7194eac0bcd7e6eed8682851548fd
anushka <anushka25999@gmail.com>
2020-10-15 10:57:47 +0530

    UC2 added in GamblerSimulator.java

--- GamblerSimulator.java
+++ GamblerSimulator.java
@@ -4,7 +4,17 @@ public class GamblerSimulator{
 	private final int TOTAL = 100;
 	private final int BET = 1;
 
-	public static void main(String args[]){
-		System.out.println("Welcome to the Gambler Simulator Problem");
+	public void stake(){
+		int random=(int)Math.floor(Math.random()*10)%2;
+		if(random==0){
+			System.out.println("You Won");
+		}
+		else{
+			System.out.println("You Lose");
+		}
+	}
+	public static void main(String[] args){
+		GamblerSimulator gamble = new GamblerSimulator();
+		gamble.stake();
 	}
 }
