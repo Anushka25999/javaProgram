f2340d866a63be720930783d9f86a7be6c460ac2
anushka <anushka25999@gmail.com>
2020-10-15 10:22:56 +0530

    UC1 added to GamblerSimulator.java

--- GamblerSimulator.java
+++ GamblerSimulator.java
@@ -1,8 +1,8 @@
 public class GamblerSimulator{
 
 	//Constants
-	final int total = 100;
-	final int bet = 1;
+	private final int TOTAL = 100;
+	private final int BET = 1;
 
 	public static void main(String args[]){
 		System.out.println("Welcome to the Gambler Simulator Problem");
